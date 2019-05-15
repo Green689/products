8a7b75b11cf40c7614d15e85304f9b66f5c4bbeb
Green <oswaldo-99@hotmail.com>
2019-05-15 08:50:32 -0600

    se relaizo el codigo que permite actualizar un producto 'updatePr.php' y 'updateProduct.php'

---
+++ updatePr.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "1234";
+$dbname = "products";
+
+$id = $_GET["id"];
+$price = $_POST["price"];
+$active = $_POST["active"];
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $sql = "UPDATE producto SET price=$price, is_active=$active WHERE id=$id";
+  $conn->query($sql);
+  echo "New record created successfully";
+  }
+catch(PDOException $e)
+  {
+  echo $sql . "<br>" . $e->getMessage();
+  }
+$conn = null;
+header("Location: index.php");
+?>
--- updateProduct.php
+++ updateProduct.php
@@ -13,19 +13,20 @@
     <div class="column is-half box">
 
       <div class="navbar">
-      <h4 class="title is-4">Editar Producto</h4>
+      <?php echo "<h4 class='title is-4'>Editar ".$_GET["name"]. "</h4>";?>
       <div class="navbar-end">
         <div class="navbar-item">
       <a class="button is-warning" onclick="location.href='index.php'">Volver</a>
       </div>
       </div>
       </div>
-      <form action="udatePr.php" method="post">
-
+      <?php
+      echo "<form action='updatePr.php?id=". $_GET["id"] ."' method='post'>";
+      ?>
         <div class="field">
           <label class="label">Price</label>
           <div class="control">
-            <input class="input" required type="text" pattern="^[0-9,.]{1,5}$" name="price" placeholder="precio">
+            <?php echo "<input value='" . $_GET["price"] . "' class='input' required type='text' pattern='^[0-9,.]{1,5}$' name='price' placeholder='precio'>";?>
           </div>
         </div>
         <div class="field">
@@ -33,8 +34,15 @@
           <div class="control">
             <div class="select">
               <select name="active">
-                <option value="true">Disponible</option>
-                <option value="false">No disponible</option>
+                <?php
+                if ($_GET["active"] == 1){
+                echo "<option value='1'>Activo</option>
+                <option value='0'>Inactivo</option>";
+              }else{
+                echo "<option value='0'>Inactivo</option>
+                <option value='1'>Activo</option>";
+              }
+                ?>
               </select>
             </div>
           </div>
